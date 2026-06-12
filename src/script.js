61d6a23b3d1c2a8471b40ecb33e9293a8c388615
miiv0 <minzana2002@gmail.com>
2026-06-12 18:37:25 -0400

    add format, add popup modal for adding task

--- src/script.js
+++ src/script.js
@@ -1,21 +1,12 @@
-const titleBox = document.getElementById("titleInput")
-const dateBox = document.getElementById("dateInput")
-const listContainer = document.getElementById("taskList")
-const error = document.getElementById('error')
+const addTaskButton = document.getElementById("addTaskButton")
+const openBtn = document.getElementById("openModal")
+const closeBtn = document.getElementById("closeModal")
+const modal = document.getElementById("modal")
 
-function addProject() {
+openBtn.addEventListener("click", () => {
+    modal.classList.add('open');
+});
 
-}
-
-function addTask() {
-    if (titleBox.value === '' || dateBox.value === '') {
-        error.innerHTML = "Missing information, Retry"
-    }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = titleBox.value + dateBox.value;
-        listContainer.appendChild(li);
-    }
-    titleBox.value = ''
-    dateBox.value = ''
-}
+closeBtn.addEventListener("click", () => {
+    modal.classList.add('close');
+});
