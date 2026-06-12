23ddfb0ef6053b5edaa89e34e9c66f225175f7a6
miiv0 <minzana2002@gmail.com>
2026-06-12 18:05:13 -0400

    add date

--- src/script.js
+++ src/script.js
@@ -1,14 +1,21 @@
-const inputBox = document.getElementById("taskInput")
+const titleBox = document.getElementById("titleInput")
+const dateBox = document.getElementById("dateInput")
 const listContainer = document.getElementById("taskList")
+const error = document.getElementById('error')
+
+function addProject() {
+
+}
 
 function addTask() {
-    if (inputBox.value === '') {
-        inputBox.placeholder = "You must enter a task!"
+    if (titleBox.value === '' || dateBox.value === '') {
+        error.innerHTML = "Missing information, Retry"
     }
     else {
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        li.innerHTML = titleBox.value + dateBox.value;
         listContainer.appendChild(li);
     }
-    inputBox.value = ''
+    titleBox.value = ''
+    dateBox.value = ''
 }
