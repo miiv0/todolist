57bd963a86bb307a2423fb13bc08fa5194aab58c
miiv0 <minzana2002@gmail.com>
2026-06-14 16:02:47 -0400

    add persistant local storage

--- src/index.js
+++ src/index.js
@@ -11,6 +11,40 @@ const taskDescInput = document.getElementById("taskDescription")
 const taskFolderInput = document.getElementById("taskFolder")
 const taskDateInput = document.getElementById("taskDate")
 
+let folders = JSON.parse(localStorage.getItem("folders")) || []
+let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+function saveFolders() {
+    localStorage.setItem("folders", JSON.stringify(folders))
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+function renderFolders() {
+    folderContainer.innerHTML = ''
+    taskFolderInput.innerHTML = '<option value="">Select a folder</option>'
+    folders.forEach(name => {
+        let li = document.createElement("li")
+        li.innerHTML = name
+        folderContainer.appendChild(li)
+
+        let option = document.createElement("option")
+        option.value = name
+        option.textContent = name
+        taskFolderInput.appendChild(option)
+    })
+}
+
+function renderTasks() {
+    tasks.forEach(task => {
+        let li = document.createElement("li")
+        li.innerHTML = task.title
+        folderContainer.appendChild(li)
+    })
+}
+
 openFolderBtn.addEventListener("click", () => {
     folderModal.classList.add("open");
 });
@@ -31,9 +65,9 @@ function addFolder() {
     if (folderInput.value === '') {
         folderInput.placeholder = "You must enter a folder name!"
     } else {
-        let li = document.createElement("li");
-        li.innerHTML = folderInput.value;
-        folderContainer.appendChild(li);
+        folders.push(folderInput.value)
+        saveFolders()
+        renderFolders()
         folderModal.classList.remove("open")
     }
     folderInput.value = ''
@@ -43,9 +77,17 @@ function addTask() {
     if (taskTitleInput.value === '') {
         taskTitleInput.placeholder = "You must enter a title!"
     } else {
-        let li = document.createElement("li");
-        li.innerHTML = taskTitleInput.value;
-        folderContainer.appendChild(li);
+        const task = {
+            title: taskTitleInput.value,
+            description: taskDescInput.value,
+            folder: taskFolderInput.value,
+            date: taskDateInput.value
+        }
+        tasks.push(task)
+        saveTasks()
+        let li = document.createElement("li")
+        li.innerHTML = task.title
+        folderContainer.appendChild(li)
         taskModal.classList.remove("open")
     }
     taskTitleInput.value = ''
@@ -53,3 +95,6 @@ function addTask() {
     taskFolderInput.value = ''
     taskDateInput.value = ''
 }
+
+renderFolders()
+renderTasks()
